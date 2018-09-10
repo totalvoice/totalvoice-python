07f7ffc95c5be250cc4b2f3ca83b79e0ffaa379f
Carlos Henrique <carlossantosdm@gmail.com>
2018-09-10 11:37:52 -0300

    setup

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     # 15.10       Date based release
     # 23          Serial release
 
-    version='1.2.0',
+    version='1.3.0',
 
     
     description='Biblioteca da Totalvoice em python',
